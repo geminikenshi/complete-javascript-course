f6c265ea1a9f36bd90719b412994436a69f46158
Kenshi Kuo <kenshi_kuo@hitrontech.com>
2023-09-26 11:17:00 +0800

    implement reset button

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,22 +2,73 @@
 
 const max = 20;
 const min = 1;
-const secretNumber = Math.ceil(Math.random() * 20);
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.ceil(Math.random() * 20);
+let score = 20;
+let highScore = 0;
+
+const messageElement = document.querySelector('.message');
+const numberElement = document.querySelector('.number');
+const scoreElement = document.querySelector('.score');
+const guessElement = document.querySelector('.guess');
+const bodyElement = document.querySelector('body');
+
+console.log('secret number:' + secretNumber);
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔️ No number!';
+    messageElement.textContent = '⛔️ No number!';
   } else if (guess < min || guess > max) {
-    document.querySelector('.message').textContent = '⚠️ Number out of bound!';
+    messageElement.textContent = '⚠️ Number out of bound!';
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🎉 Correct number!';
+    // Correct number
+    messageElement.textContent = '🎉 Correct number!';
+    numberElement.textContent = secretNumber;
+
+    bodyElement.style.backgroundColor = '#60b347';
+    numberElement.style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Number too high!';
+    checkScore('📈 Number too high!');
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 Number too low!';
+    checkScore('📉 Number too low!');
   }
+});
+
+function checkScore(message) {
+  if (score > 1) {
+    messageElement.textContent = message;
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else {
+    messageElement.textContent = '🫤 You lost!';
+    document.querySelector('.score').textContent = 0;
+  }
+}
+
+document.querySelector('.again').addEventListener('click', function () {
+  // reset score
+  score = 20;
+  scoreElement.textContent = score;
+
+  // regenerate secret number
+  secretNumber = Math.ceil(Math.random() * 20);
+  console.log('secret number:' + secretNumber);
+
+  // reset message
+  messageElement.textContent = 'Start guessing...';
+
+  // reset input form
+  guessElement.value = '';
+
+  // reset style
+  bodyElement.style.backgroundColor = '#222';
+  numberElement.textContent = '?';
+  numberElement.style.width = '15rem';
 });
