2b9f84536a5e1067a2d1db3d88536e3f84278091
Kenshi Kuo <kenshi_kuo@hitrontech.com>
2023-10-17 17:39:34 +0800

    geo location

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -9,7 +9,40 @@ const countriesContainer = document.querySelector('.countries');
 
 const apiRoot = 'https://countries-api-836d.onrender.com/countries';
 
-const getCountryData = function (country) {
+const renderCountry = function (data, classname = '') {
+  const html = `
+    <article class="country ${classname}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}M</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `${apiRoot}/name/${country}`);
+//   request.send();
+
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+
+//     renderCountry(data);
+//   });
+// };
+
+const getCountryAndNeighbour = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `${apiRoot}/name/${country}`);
   request.send();
@@ -18,26 +51,59 @@ const getCountryData = function (country) {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
 
-    const html = `
-  <article class="country">
-  <img class="country__img" src="${data.flag}" />
-  <div class="country__data">
-    <h3 class="country__name">${data.name}</h3>
-    <h4 class="country__region">${data.region}</h4>
-    <p class="country__row"><span>👫</span>${(
-      +data.population / 1000000
-    ).toFixed(1)}M</p>
-    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-  </div>
-</article>
-`;
+    // render country
+    renderCountry(data);
 
-    countriesContainer.insertAdjacentHTML('afterbegin', html);
-    countriesContainer.style.opacity = 1;
+    // render neighbor
+    const neighbour = data.borders?.[0];
   });
 };
 
-getCountryData('gb');
-getCountryData('sk');
-getCountryData('greece');
+// getCountryData('sk');
+// getCountryData('greece');
+
+const getCountryData = function (country) {
+  fetch(`${apiRoot}/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbor = data[0].borders?.[0];
+      return fetch(`${apiRoot}/alpha/${neighbor}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
+};
+
+getCountryData('cyprus');
+
+// console.log('Test start');
+// setTimeout(() => console.log('0 sec timer'), 0);
+// Promise.resolve('Resolved promise 1').then(res => console.log(res));
+// console.log('Test end');
+
+// const lotterPromise = new Promise(function (resolve, reject) {
+//   if (Math.random() >= 0.5) {
+//     resolve('You WIN');
+//   } else {
+//     reject('You lost your money');
+//   }
+// });
+
+// lotterPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// navigator.geolocation.getCurrentPosition(
+//   location => console.log(location),
+//   err => console.error(err)
+// );
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+getPosition().then(pos => console.log(pos));
+
+const whereAmI = function (lat, lng) {
+  fetch(`https://geocode.xyz`);
+};
