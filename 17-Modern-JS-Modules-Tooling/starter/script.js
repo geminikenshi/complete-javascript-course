4016e10465d0c11443606b4413d28b6669299006
Kenshi Kuo <kenshi_kuo@hitrontech.com>
2023-10-24 11:56:41 +0800

    use module

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,3 +1,21 @@
 // Importing module
 import './shoppingCart.js';
 console.log('Importing module');
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const agent = {
+  name: {
+    first: 'James',
+    last: 'Cool',
+  },
+  information: {
+    bloodtype: 'O',
+    age: 35,
+  },
+};
+
+const agentClone = cloneDeep(agent);
+agent.information.age -= 10;
+
+console.log(agent);
+console.log(agentClone);
